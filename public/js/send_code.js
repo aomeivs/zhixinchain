afe126c70c0352103c28e2a543166cb6b68812fe
zhou <diyuqianxiao@26.com>
2018-01-08 18:01:23 +0800

    职信链改版相关，优惠券IE10+兼容支持（图片下载为pdf格式）

---
+++ public/js/send_code.js
@@ -0,0 +1,58 @@
+/**
+ * Created by zhou on 17/2/13.
+ */
+/***
+ ~~~~~~~~~~~~~~~~~~~~~~~~验证码获取~~~~~~~~~~~~~~~~~~~~~~~
+ **/
+
+$(function () {
+    var InterValObj; //timer变量，控制时间
+    var count = 5; //间隔函数，1秒执行
+    var curCount;//当前剩余秒数
+    var $ht_yanzheng = $(".auth-code");//验证按钮
+
+    function sendMessage() {
+        curCount = count;
+        //设置button效果，开始计时
+        $ht_yanzheng.attr("disabled", "true");
+        $ht_yanzheng.addClass('actived')
+        $ht_yanzheng.text("5分钟内有效(" + curCount +")" );
+        InterValObj = window.setInterval(SetRemainTime, 1000); //启动计时器，1秒执行一次
+        //向后台发送处理数据
+        //$.get("/account/ajax_sendSMS?phone="+$("#h1").val(),function(result_data){
+        $("#vcode_c").val(111111)//result_data);
+        // });
+
+        /*
+         $.ajax({
+         　　type: "POST", //用POST方式传输
+         　　dataType: "text", //数据格式:JSON
+         　　url: '', //目标地址
+         　　 data: "",
+         　　 error: function (XMLHttpRequest, textStatus, errorThrown) { },
+         　　success: function (msg){ }
+         });
+         */
+    }
+
+//timer处理函数
+    function SetRemainTime() {
+        if (curCount == 0) {
+            window.clearInterval(InterValObj);//停止计时器
+            $ht_yanzheng.removeAttr("disabled");//启用按钮
+            $ht_yanzheng.removeClass('actived')
+            $ht_yanzheng.text("重新发送验证码");
+        }
+        else {
+            curCount--;
+            $ht_yanzheng.text("5分钟内有效(" + curCount +")");
+        }
+    }
+
+    $ht_yanzheng.click(function(){
+        //if($("#u_phone").val()==""){ layer.msg("请输入手机号！");return;}
+        //$("#u_phone").attr("disabled", "true");
+        sendMessage();
+        console.log('sendMessage pass')
+    })
+})
